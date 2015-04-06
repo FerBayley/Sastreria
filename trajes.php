86ec77552516880f6ea8707161d0b817a66f2389
fernando.bayley@gmail.com <fernando.bayley@gmail.com>
2015-04-06 01:09:43 -0300

    Organizacion de ambos

--- trajes.php
+++ trajes.php
@@ -25,76 +25,8 @@ Template Name: trajes
 		<h2>Trajes</h2>
 	</section> <!-- End of Cuerpo -->
 
-	<section class="Contenido">
-		<div class="Col1">
-			<div class="Emboltorio">
-				<?php query_posts("category_name=trajes-col1"); ?>
-				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-					<h2><?php the_title();?></h2>
-
-					<div class="Thumb">
-						<?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'list_articles_thumbs' ); } ?>
-					</div><!-- End of Thumb -->
-
-					<p><?php the_content();?></p>
-				<?php endwhile; else: ?>
-					<p>No se encontraron productos cargados en esta sección.</p>
-				<?php endif; ?>
-			</div> <!-- End of Emboltorio -->
-		</div> <!-- End of Col1 -->
-
-		<div class="Col2">
-			<div class="Emboltorio">
-				<?php query_posts("category_name=trajes-col2"); ?>
-				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-					<h2><?php the_title();?></h2>
-					
-					<div class="Thumb">
-						<?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'list_articles_thumbs' ); } ?>
-					</div><!-- End of Thumb -->
-
-					<p><?php the_content();?></p>
-				<?php endwhile; else: ?>
-					<p>No se encontraron productos cargados en esta sección.</p>
-				<?php endif; ?>
-			</div> <!-- End of Emboltorio -->
-		</div> <!-- End of Col2 -->
-
-		<div class="Col3">
-			<div class="Emboltorio">
-				<?php query_posts("category_name=trajes-col3"); ?>
-				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-					<h2><?php the_title();?></h2>
-					
-					<div class="Thumb">
-						<?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'list_articles_thumbs' ); } ?>
-					</div><!-- End of Thumb -->
-
-					<p><?php the_content();?></p>
-				<?php endwhile; else: ?>
-					<p>No se encontraron productos cargados en esta sección.</p>
-				<?php endif; ?>
-			</div> <!-- End of Emboltorio -->
-		</div> <!-- End of Col3 -->
-
-		<div class="Col4">
-			<div class="Emboltorio">
-				<?php query_posts("category_name=trajes-col4"); ?>
-				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-					<h2><?php the_title();?></h2>
-					
-					<div class="Thumb">
-						<?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'list_articles_thumbs' ); } ?>
-					</div><!-- End of Thumb -->
-
-					<p><?php the_content();?></p>
-				<?php endwhile; else: ?>
-					<p>No se encontraron productos cargados en esta sección.</p>
-				<?php endif; ?>
-			</div> <!-- End of Emboltorio -->
-		</div> <!-- End of Col4 -->
-	</section> <!-- End of Contenido -->
-
+	<?php include ("includes/columna-100.php"); ?>
+	<?php include ("includes/columna-130.php"); ?>
 	<?php include("includes/pie-interno.php"); ?>
 
 </body>
