7162d8ecc8b6b35b9e012189b7ff456850c5bb63
fernando.bayley@gmail.com <fernando.bayley@gmail.com>
2015-04-06 03:17:14 -0300

    Correccion archivos corbatas.php

--- corbatas.php
+++ corbatas.php
@@ -25,75 +25,23 @@ Template Name: corbatas
 		<h2>Corbatas</h2>
 	</section> <!-- End of Cuerpo -->
 
-	<section class="Contenido">
-		<div class="Col1">
-			<div class="Emboltorio">
-				<?php query_posts("category_name=corbatas-col1"); ?>
-				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-					<h2><?php the_title();?></h2>
+	<section class="Trajes__cien">
+		<h3>Corbatas Detalí</h3>
+	</section> <!-- End of 100 -->
 
-					<div class="Thumb">
-						<?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'list_articles_thumbs' ); } ?>
-					</div><!-- End of Thumb -->
+	<?php include("includes/corbatas-detali.php"); ?>
 
-					<p><?php the_content();?></p>
-				<?php endwhile; else: ?>
-					<p>No se encontraron productos cargados en esta sección.</p>
-				<?php endif; ?>
-			</div> <!-- End of Emboltorio -->
-		</div> <!-- End of Col1 -->
+	<section class="Trajes__cien">
+		<h3>Corbatas Christian Lacroix</h3>
+	</section> <!-- End of 100 -->
 
-		<div class="Col2">
-			<div class="Emboltorio">
-				<?php query_posts("category_name=corbatas-col2"); ?>
-				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-					<h2><?php the_title();?></h2>
+	<?php include("includes/corbatas-lacroix.php"); ?>
 
-					<div class="Thumb">
-						<?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'list_articles_thumbs' ); } ?>
-					</div><!-- End of Thumb -->
+	<section class="Trajes__cien">
+		<h3>Corbatas G. Redaelli seda italiana</h3>
+	</section> <!-- End of 100 -->
 
-					<p><?php the_content();?></p>
-				<?php endwhile; else: ?>
-					<p>No se encontraron productos cargados en esta sección.</p>
-				<?php endif; ?>
-			</div> <!-- End of Emboltorio -->
-		</div> <!-- End of Col2 -->
-
-		<div class="Col3">
-			<div class="Emboltorio">
-				<?php query_posts("category_name=corbatas-col3"); ?>
-				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-					<h2><?php the_title();?></h2>
-
-					<div class="Thumb">
-						<?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'list_articles_thumbs' ); } ?>
-					</div><!-- End of Thumb -->
-
-					<p><?php the_content();?></p>
-				<?php endwhile; else: ?>
-					<p>No se encontraron productos cargados en esta sección.</p>
-				<?php endif; ?>
-			</div> <!-- End of Emboltorio -->
-		</div> <!-- End of Col3 -->
-
-		<div class="Col4">
-			<div class="Emboltorio">
-				<?php query_posts("category_name=corbatas-col4"); ?>
-				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-					<h2><?php the_title();?></h2>
-
-					<div class="Thumb">
-						<?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'list_articles_thumbs' ); } ?>
-					</div><!-- End of Thumb -->
-
-					<p><?php the_content();?></p>
-				<?php endwhile; else: ?>
-					<p>No se encontraron productos cargados en esta sección.</p>
-				<?php endif; ?>
-			</div> <!-- End of Emboltorio -->
-		</div> <!-- End of Col4 -->
-	</section> <!-- End of Contenido -->
+	<?php include("includes/corbatas-redaelli.php"); ?>
 
 	<?php include("includes/pie-interno.php"); ?>
 
