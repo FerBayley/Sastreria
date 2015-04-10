6f47c41533c71800fb8ba4744b679eb1e1a829c5
fernando.bayley@gmail.com <fernando.bayley@gmail.com>
2015-04-10 16:22:55 -0300

    Cambios en las galerias

--- includes/camisas-rochas.php
+++ includes/camisas-rochas.php
@@ -1,10 +1,27 @@
-<section class="Contenido">		
+<section class="Contenido">
 		<div class="Col1">
 			<div class="Emboltorio">
-				<?php query_posts("category_name=trajes100-col1"); ?>
+				<?php query_posts("category_name=camisas-rochas-col1"); ?>
 				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 					<h2><?php the_title();?></h2>
-					
+
+					<div class="Thumb">
+						<?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'list_articles_thumbs' ); } ?>
+					</div><!-- End of Thumb -->
+
+					<p><?php the_content();?></p>
+				<?php endwhile; else: ?>
+					<p>No se encontraron productos cargados en esta sección.</p>
+				<?php endif; ?>
+			</div> <!-- End of Emboltorio -->
+		</div> <!-- End of Col1 -->
+
+		<div class="Col2">
+			<div class="Emboltorio">
+				<?php query_posts("category_name=camisas-rochas-col2"); ?>
+				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+					<h2><?php the_title();?></h2>
+
 					<div class="Thumb">
 						<?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'list_articles_thumbs' ); } ?>
 					</div><!-- End of Thumb -->
@@ -16,12 +33,12 @@
 			</div> <!-- End of Emboltorio -->
 		</div> <!-- End of Col2 -->
 
-		<div class="Col2">
+		<div class="Col3">
 			<div class="Emboltorio">
-				<?php query_posts("category_name=trajes100-col2"); ?>
+				<?php query_posts("category_name=camisas-rochas-col3"); ?>
 				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 					<h2><?php the_title();?></h2>
-					
+
 					<div class="Thumb">
 						<?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'list_articles_thumbs' ); } ?>
 					</div><!-- End of Thumb -->
@@ -33,29 +50,12 @@
 			</div> <!-- End of Emboltorio -->
 		</div> <!-- End of Col3 -->
 
-		<div class="Col3">
-			<div class="Emboltorio">
-				<?php query_posts("category_name=trajes100-col3"); ?>
-				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-					<h2><?php the_title();?></h2>
-					
-					<div class="Thumb">
-						<?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'list_articles_thumbs' ); } ?>
-					</div><!-- End of Thumb -->
-
-					<p><?php the_content();?></p>
-				<?php endwhile; else: ?>
-					<p>No se encontraron productos cargados en esta sección.</p>
-				<?php endif; ?>
-			</div> <!-- End of Emboltorio -->
-		</div> <!-- End of Col4 -->
-
 		<div class="Col4">
 			<div class="Emboltorio">
-				<?php query_posts("category_name=trajes100-col4"); ?>
+				<?php query_posts("category_name=camisas-rochas-col4"); ?>
 				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 					<h2><?php the_title();?></h2>
-					
+
 					<div class="Thumb">
 						<?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'list_articles_thumbs' ); } ?>
 					</div><!-- End of Thumb -->
